9e4c865679c9716be767359d3e7e3dc6a586f44b
SAEED <humoyun@Saeeds-MacBook-Pro.local>
2024-09-30 18:06:49 +0500

    First commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by SAEED on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
